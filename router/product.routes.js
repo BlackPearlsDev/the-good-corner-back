9959b4e969e6480b6f1158bb13cab3018df41930
BlackPearlsDev <proeslros2@gmail.com>
2022-09-08 17:06:01 +0200

    better route organization

--- router/product.routes.js
+++ router/product.routes.js
@@ -3,16 +3,20 @@ const router = express.Router();
 
 import { getAllProduct, getProductById, getProductByCategoryId, addProduct, addCategory, updateProductbyId, updateCategorybyId, getAllCategory, addImg, addImgByProductId } from '../controllers/product.controller.js';
 
-// ici on est sur l'url http://localhost:9000/
+// GET
 router.get("/", getAllProduct);
 router.get("/category/:id", getProductByCategoryId);
+router.get("/category", getAllCategory);
+router.get("/:id", getProductById);
+
+// POST
 router.post("/addProduct", addProduct);
 router.post("/addCategory", addCategory);
 router.put("/updateProduct/:id", updateProductbyId);
-router.put("/updateCategory/:id", updateCategorybyId);
-router.get("/category", getAllCategory);
 router.post("/addImg", addImg);
+
+// PUT
+router.put("/updateCategory/:id", updateCategorybyId);
 router.put("/updateImg/:id", addImgByProductId);
-router.get("/:id", getProductById);
 
 export default router;
--- router/user.routes.js
+++ router/user.routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 import { getAllUser, addUser, getUserByUuid, updateUserbyUuid } from '../controllers/user.controller.js';
 
-// ici on est sur l'url http://localhost:9000/
+// GET
 router.get("/", getAllUser);
-router.post("/", addUser);
 router.get("/:uuid", getUserByUuid);
+
+// POST
+router.post("/", addUser);
+
+// PUT
 router.put("/updateUser/:uuid", updateUserbyUuid);
 
 export default router;
